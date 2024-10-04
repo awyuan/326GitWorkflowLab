8db502720104e47dbae22c09ae3f6f2d061de133
Yuni Jeong <syjeong215@gmail.com>
2024-10-04 12:48:15 -0400

    fixed reciprocal

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,21 +1,23 @@
-const assert = require('assert');
-const calculator = require('../calculator');
+const assert = require("assert");
+const calculator = require("../calculator");
 
 // Test memory functions
-describe('Calculator Memory Functions', function() {
-    
-});
-
+describe("Calculator Memory Functions", function () {});
 
 // Test calculation functions
-describe('Calculator Calculation Functions', function() {
-    const res = calculator.calculateSquareRoot(4);
-    it('should return 2', function() {
-        assert.strictEqual(res, 2);
-    });
-    // Doesn't work!
-    const res2 = calculator.calculateSquare(2)
-    it('should return 4', function() {
-        assert.strictEqual(res2, 4);
-    });
+describe("Calculator Calculation Functions", function () {
+	const res = calculator.calculateSquareRoot(4);
+	it("should return 2", function () {
+		assert.strictEqual(res, 2);
+	});
+	// Doesn't work!
+	const res2 = calculator.calculateSquare(2);
+	it("should return 4", function () {
+		assert.strictEqual(res2, 4);
+	});
+
+	const res3 = calculator.calculateReciprocal(10);
+	it("should return 0.1", function () {
+		assert.strictEqual(res3, 0.1);
+	});
 });
